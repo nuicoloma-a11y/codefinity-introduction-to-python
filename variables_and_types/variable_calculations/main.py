a291e7757e5a8a7c807b91e97fbeb5da5b41c301
nuicoloma-a11y <>
2026-07-31 07:47:48 +0000

    Variable calculations

---
+++ variables_and_types/variable_calculations/main.py
@@ -0,0 +1,7 @@
+purchase_quantity = 5
+item_name = "Soda" 
+item_price = 6.99
+total_cost = item_price * purchase_quantity
+
+# Testing
+print("Total cost for", purchase_quantity, "cases of", item_name, "is $", total_cost)
